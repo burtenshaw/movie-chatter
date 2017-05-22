7ae8074cebc98c106b511028f404e9e32170ec41
Ali Doku <ali.doku1@gmail.com>
2017-05-23 00:47:49 +0200

    minor bug fix in aboutAdapter

--- test.py
+++
@@ -1,8 +0,0 @@
-from utils import nlp
-from pyjarowinkler import distance
-
-word1 = "music"
-word2 = 'comedy'
-
-print distance.get_jaro_distance(word1, word2)
-print nlp.levensteinWord(word1, word2)
